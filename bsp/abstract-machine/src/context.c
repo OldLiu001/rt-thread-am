5f346d2de3e273437403aca22ce0fda85541fed1
OldLiu <632171029@qq.com>
2025-04-12 11:47:08 +0800

    rt_hw_stack_init() finish without testing

--- bsp/abstract-machine/src/context.c
+++ bsp/abstract-machine/src/context.c
@@ -2,6 +2,15 @@
 #include <klib.h>
 #include <rtthread.h>
 
+// OldLiu Modify Start
+#define STACK_SIZE (4096 * 8)
+typedef union {
+  uint8_t stack[STACK_SIZE];
+  struct { Context *cp; };
+} PCB;
+static PCB pcb[2], pcb_boot, *current = &pcb_boot;
+// OldLiu Modify End
+
 static Context* ev_handler(Event e, Context *c) {
   switch (e.event) {
     default: printf("Unhandled event ID = %d\n", e.event); assert(0);
@@ -14,7 +23,10 @@ void __am_cte_init() {
 }
 
 void rt_hw_context_switch_to(rt_ubase_t to) {
+  // OldLiu Modify Start
   assert(0);
+
+  // OldLiu Modify End
 }
 
 void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
@@ -26,6 +38,14 @@ void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t t
 }
 
 rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_addr, void *texit) {
-  assert(0);
-  return NULL;
+  // OldLiu Modify Start
+  // assert(0);
+  assert(current);
+  rt_uint8_t *my_stack_addr = (rt_uint8_t *)((uintptr_t)stack_addr & ~(sizeof(uintptr_t) - 1));
+
+  pcb[0].cp = kcontext((Area) { my_stack_addr, my_stack_addr + STACK_SIZE}, tentry, parameter);
+  // yield();
+  // panic("Should not reach here!");
+  return (rt_uint8_t *)pcb[0].cp;
+  // OldLiu Modify End
 }
